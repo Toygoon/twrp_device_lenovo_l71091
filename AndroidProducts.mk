861a2eb87fd71b3819bb001aadb7469b02357102
Toygoon <toygoon@gmail.com>
2024-03-02 15:12:25 +0900

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_halo.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_halo-user \
+    omni_halo-userdebug \
+    omni_halo-eng
